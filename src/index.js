8be6656ac94bf81681011e896f874fd4635185e7
Konaschkov <konashkov23@gmail.com>
2023-10-02 20:08:58 +0200

    Rendering ip info

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import {validatIp} from './helpers';
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
 
+const ipInfo = document.querySelector('#ip');
+const locationInfo = document.querySelector('#location');
+const timezoneInfo = document.querySelector('#timezone');
+const ispInfo = document.querySelector('#isp');
+
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
 
 function getData() {
     if(validatIp(ipInput.value)) {
-        fetch(`https://geo.ipify.org/api/v1?apiKey=at_1dqFV88UBSqsVUUIqC8LRtfGN0Z3Q&ipAddress=${ipInput.value}`).then(response => response.json()).then(console.log)
+        fetch(`https://geo.ipify.org/api/v1?apiKey=at_1dqFV88UBSqsVUUIqC8LRtfGN0Z3Q&ipAddress=${ipInput.value}`).then(response => response.json()).then(setInfo)
     }
 }
 
@@ -16,4 +21,13 @@ function handleKey(e) {
     if(e.key === 'Enter') {
         getData();
     }
+}
+
+function setInfo(mapData) {
+    const {ip, location, isp} = mapData;
+
+    ipInfo.innerText = ip;
+    locationInfo.innerText = location.country + ' ' + location.region;
+    timezoneInfo.innerText = location.timezone;
+    ispInfo.innerText = isp;
 }
