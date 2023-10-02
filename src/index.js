d199a3c77be7ae9618319ba75f11a20d36335f3e
Konaschkov <konashkov23@gmail.com>
2023-10-02 19:24:02 +0200

    Add parcel, add get data ip

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const ipInput = document.querySelector('.search-bar__input');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', getData);
+ipInput.addEventListener('keydown', handleKey);
+
+function getData() {
+    fetch(`https://geo.ipify.org/api/v1?apiKey=at_1dqFV88UBSqsVUUIqC8LRtfGN0Z3Q&ipAddress=${ipInput.value}`).then(response => response.json()).then(console.log)
+}
+
+function handleKey(e) {
+    if(e.key === 'Enter') {
+        getData();
+    }
+}
