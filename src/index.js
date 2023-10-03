de68f355d560398276e0e4f7baeaee49ebc75873
Konaschkov <konashkov23@gmail.com>
2023-10-03 20:08:16 +0200

    Add map

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import {validatIp} from './helpers';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import icon from '../images/icon-location.svg';
 
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
@@ -7,6 +10,8 @@ const ipInfo = document.querySelector('#ip');
 const locationInfo = document.querySelector('#location');
 const timezoneInfo = document.querySelector('#timezone');
 const ispInfo = document.querySelector('#isp');
+
+const mapArea = document.querySelector('.map');
 
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
@@ -30,4 +35,25 @@ function setInfo(mapData) {
     locationInfo.innerText = location.country + ' ' + location.region;
     timezoneInfo.innerText = location.timezone;
     ispInfo.innerText = isp;
+
+    map.setView([location.lat, location.lng]);
+    L.marker([location.lat, location.lng], {icon: markerIcon}).addTo(map);
 }
+
+const map = L.map(mapArea, {
+    center: [51.505, -0.09],
+    zoom: 13,
+    zoomControl: false,
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+const markerIcon = L.icon({
+    iconUrl: icon,
+    iconSize: [30, 40],
+});
+
+L.marker([51.505, -0.09], {icon: markerIcon}).addTo(map);
